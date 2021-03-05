1b1f7c421ec935625ff6b8d12afc86da0771b318
kanine <keith.pickard@gmail.com>
2021-03-05 17:04:26 +1100

    autocomplete Demo

---
+++ autcompleteBS/js/autocompleteBSDemo.js
@@ -0,0 +1,33 @@
+const autoCompleteConfig = [{
+    name: 'Countries by Capital',
+    debounceMS: 250,
+    minLength: 3,
+    maxResults: 10,
+    inputSource: document.getElementById('inputText1'),
+    targetID: document.getElementById('inputID1'),
+    fetchURL: 'https://restcountries.eu/rest/v2/capital/{term}',
+    fetchMap: {id: "alpha2Code",
+               name: "capital"}
+  },
+  {
+    name: 'Countries by Name',
+    debounceMS: 300,
+    minLength: 3,
+    maxResults: 10,
+    inputSource: document.getElementById('inputText2'),
+    targetID: document.getElementById('inputID2'),
+    fetchURL: 'https://restcountries.eu/rest/v2/name/{term}',
+    fetchMap: {id: "alpha2Code",
+               name: "name"}
+  }
+];
+
+console.log(autoCompleteConfig);
+
+// Initiate Autocomplete to Create Listeners
+autocompleteBS(autoCompleteConfig);
+
+function resultHandlerBS(inputName, selectedData) {
+  console.log(inputName);
+  console.log(selectedData);
+}
